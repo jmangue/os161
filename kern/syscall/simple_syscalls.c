f479ddd080ac9ee3b32ca5bfd475e9b59d9e13a7
jmangue <justin.mangue@gmail.com>
2014-03-09 15:07:40 -0700

    Added printstring()

--- kern/syscall/simple_syscalls.c
+++ kern/syscall/simple_syscalls.c
@@ -18,3 +18,12 @@ int sys_printint(int i)
 {
 	return kprintf("%d",i);
 }
+
+int sys_printstring(char* s, size_t len)
+{
+	char s_safe[len];
+	size_t string_length;
+	copyinstr((const_userptr_t) s, s_safe, len, &string_length);
+	kprintf(s_safe);
+	return string_length;
+}
